8ac4fea656d03162e9350de75352b778e59e8e50
Hung Tang <hungtang@hotmail.com>
2021-03-06 14:10:02 -0800

    incorporated time comparison to determine past, present and future

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,11 @@
+// Wait till DOM ready ****************
 $( document ).ready(function() {
+
+    //Initializing variables ------------------
+    const past = "past";
+    const present = "present";
+    const future = "future";
+
     //var timeBlockContainer = document.querySelector(".container");
     let timeBlockContainer = $('.container');
 
@@ -6,42 +13,16 @@ $( document ).ready(function() {
     var currentDate = moment().format("dddd, MMMM Do");
     $("#currentDay").html(currentDate);
 
-    var currentHour = moment().format("HH");
+    var currentHour = parseInt(moment().format("HH"));
 
-    // Determine if currentHour is AM or PM
-    //var isAmOrPm = (currentHour >= 12)? "PM" : "AM";
 
-    // var showHour = (hour) => {
-    //     if (hour >= 12) {
-    //         return `${hour}PM`;
-    //     } else {
-    //         return `${hour}AM`;
-    //     }
-    // };
-
-    // var timeCol = [];
-    // var tasksCol = [];
-    // var saveCol = [];
-    // var buildRowColumns = (i) => {
-    //         //Function to return hour in AM/PM
-    //         var showTime = showHour(i);
-    //         timeCol.push(`<div class='col mb-1 hour' id='time-${i}'>${showTime}</div>`);
-    //         //tasksCol.push()
-
-    //     $(".row").append(timeCol.join(""));
-
-    // }
-
-    // Build hourly rows starting from 0900 (9AM) hour to 1700 hour (5PM)
+    // Build hourly parent rows starting from 0900 (9AM) hour to 1700 hour (5PM)
     var hourlyRows = [];
     for (let i = 8; i <= 17; i++) {
         hourlyRows.push("<div class='row row-hour" + i + "'></div>");
-
-        //Build each of the columns within the hourly row container
-        //buildRowColumns(i);
-
     }
     timeBlockContainer.append(hourlyRows.join(""));
+
 
     // Determine if hour is AM/PM and return hour in AM/PM accordingly
     var showHour = (hour) => {
@@ -54,28 +35,44 @@ $( document ).ready(function() {
         }
     };
 
-    // code block to create each of the time/tasks/save columns per row
+    // code block to create each of the time/tasks/save columns per row-hour
     var timeCol = [];
     var tasksCol = [];
     var saveCol = [];
-    var buildRowColumns = () => {
-        for (let i = 8; i <= 17; i++) {
-            //Function to return hour in AM/PM
-            var showTime = showHour(i);
-            timeCol.push(`<div class="col mb-1 hour" id="time-${i}">${showTime}</div>`);
-            tasksCol.push(`<div class="col-10 mb-1 description" id="taskInput-${i}"></div>`);
-            saveCol.push(`<div class="col mb-1 saveBtn" id="saveBtn-${i}"></div>`);
 
-            $(`".row-hour${i}`).append(timeCol[i+8]);
-            // $(".row").append(tasksCol.join(""));
-            // $(".row").append(saveCol.join(""));
+    // Set default timeHorizon to future
+    var timeHorizon = future;
 
+    for (let i = 8; i <= 17; i++) {
+
+        // Assign and call function to return hour in AM/PM
+        var showTime = showHour(i);
+        timeCol.push(`<div class="col mb-1 hour" id="time-${i}">${showTime}</div>`);
+        tasksCol.push(`<div class="col-10 mb-1 description" id="taskInput-${i}"></div>`);
+        saveCol.push(`<div class="col mb-1 saveBtn" id="saveBtn-${i}"></div>`);
+
+        $(".row-hour"+i).append(timeCol[i-8]);
+        $(".row-hour"+i).append(tasksCol[i-8]);
+        $(".row-hour"+i).append(saveCol[i-8]);
+
+        // Determine time horizon: present, past, or future
+        if(i === currentHour) {
+            timeHorizon = present;
+        } else if (i > currentHour) {
+            timeHorizon = future;
+        } else {
+            timeHorizon = past;
         }
 
-        // $(".row").append(timeCol.join(""));
-        // $(".row").append(tasksCol.join(""));
-        // $(".row").append(saveCol.join(""));
+        $("#taskInput-"+i).append(`<textarea class="form-control ${timeHorizon}"></textarea>`);
+        $("#saveBtn-"+i).append(`<i class='far fa-save' id='btnImage-${i}'></i>`);
+    
     }
 
-    buildRowColumns();
+   
+
+
+
+
+
 });
