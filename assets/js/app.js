e4f9f8bf77169cc58fedc224fcc8694b2e52ac80
Hung Tang <hungtang@hotmail.com>
2021-03-06 18:21:13 -0800

    added test code to extract textarea input from click event

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,7 +16,7 @@ $( document ).ready(function() {
     var currentHour = parseInt(moment().format("HH"));
 
 
-    // Build hourly parent rows starting from 0900 (9AM) hour to 1700 hour (5PM)
+    // Build hourly parent containers starting from 0900 (9AM) hour to 1700 hour (5PM)
     var hourlyRows = [];
     for (let i = 8; i <= 17; i++) {
         hourlyRows.push("<div class='row row-hour" + i + "'></div>");
@@ -47,7 +47,7 @@ $( document ).ready(function() {
 
         // Assign and call function to return hour in AM/PM
         var showTime = showHour(i);
-        timeCol.push(`<div class="col mb-1 hour" id="time-${i}">${showTime}</div>`);
+        timeCol.push(`<div class="col mb-1 hour" id="hour-${i}">${showTime}</div>`);
         tasksCol.push(`<div class="col-10 mb-1 description" id="taskInput-${i}"></div>`);
         saveCol.push(`<div class="col mb-1 saveBtn" id="saveBtn-${i}"></div>`);
 
@@ -64,15 +64,44 @@ $( document ).ready(function() {
             timeHorizon = past;
         }
 
-        $("#taskInput-"+i).append(`<textarea class="form-control ${timeHorizon}"></textarea>`);
+        $("#taskInput-"+i).append(`<textarea class="form-control ${timeHorizon}" id="text-${i}"></textarea>`);
         $("#saveBtn-"+i).append(`<i class='far fa-save' id='btnImage-${i}'></i>`);
     
     }
 
-   
+    var test = [];
 
+    // Listen for save button click and write object to array
+    for (let i=8; i <= 17; i++) {
+        $("#saveBtn-"+i).click(function () {
+            console.log(`Button ${i} has been pushed`);
+            var textInput = document.getElementById("#text-"+i).nodeValue;
+            console.log(textInput);
+        });
+    }
 
+    // Write object to localStorage
+    var workdayScheduleObj = [
+        {
+        time: "9am",
+        task: "what I do at 9am"
+        },
+        {time: "10am",
+        task: "Eat breakfast"
+        }
+    ];
+    console.log(`${workdayScheduleObj[0].time}, rise and shine!  Time to do your ${workdayScheduleObj[1].task}.`);
 
+    var saveSchedule = () => {
+        localStorage.setItem("schedule", JSON.stringify(workdayScheduleObj));
+    }
 
+    // write user input to a variable and put into an array object
+    // var taskToObj = function() {
+        
+    //     var taskInput = $("
 
+    // }
+
+    saveSchedule();
 });
